c6c6c116dcc02752cb52bed962e6be7f67967b08
renothing <cncaiker@qq.com>
2017-12-19 13:05:01 +0800

    modify pool settings via entrypoint

--- run.sh
+++ run.sh
@@ -7,16 +7,17 @@
 #
 v=`php -r "echo PHP_MAJOR_VERSION;"`
 pconf=/etc/php${v}/php.ini
-fconf=/etc/php${v}/php-fpm.conf
+fconf=/etc/php${v}/php-fpm.conf /etc/php${V}/php-fpm
+fconf1=/etc/php${V}/php-fpm.d/www.conf
 sed -i "s|;\s*emergency_restart_threshold\s*=.*|emergency_restart_threshold =${FPM_RESTART_THRESHOLD}|i" $fconf && \
 sed -i "s|;\s*emergency_restart_interval\s*=.*|emergency_restart_interval =${FPM_RESTART_INTERVAL}|i" $fconf && \
 sed -i "s|;\s*process.max\s*=.*|process.max =${FPM_MAX}|i" $fconf && \
-sed -i "s|;*pm.max_children\s*=.*|pm.max_children =${FPM_MAX}|i" $fconf && \
-sed -i "s|pm.start_servers\s*=.*|;pm.start_servers = 2|i" $fconf && \
-sed -i "s|pm.max_spare_servers\s*=.*|pm.max_spare_servers =${FPM_MAX}|i" $fconf && \
-sed -i "s|;\s*pm.max_requests\s*=.*|pm.max_requests =${FPM_MAX_REQUESTS}|i" $fconf && \
-sed -i "s|;*request_terminate_timeout\s*=.*|request_terminate_timeout = ${PHP_MAX_EXECUTION}|i" $fconf && \
-sed -i "s|;*request_slowlog_timeout\s*=.*|request_slowlog_timeout = ${FPM_SLOW_TIME}|i" $fconf && \
+sed -i "s|;*pm.max_children\s*=.*|pm.max_children =${FPM_MAX}|i" $fconf1 && \
+sed -i "s|pm.start_servers\s*=.*|;pm.start_servers = 2|i" $fconf1 && \
+sed -i "s|pm.max_spare_servers\s*=.*|pm.max_spare_servers =${FPM_MAX}|i" $fconf1 && \
+sed -i "s|;\s*pm.max_requests\s*=.*|pm.max_requests =${FPM_MAX_REQUESTS}|i" $fconf1 && \
+sed -i "s|;*request_terminate_timeout\s*=.*|request_terminate_timeout = ${PHP_MAX_EXECUTION}|i" $fconf1 && \
+sed -i "s|;*request_slowlog_timeout\s*=.*|request_slowlog_timeout = ${FPM_SLOW_TIME}|i" $fconf1 && \
 sed -i "s|date.timezone\s*=.*|date.timezone = ${TIMEZONE}|i" $pconf && \
 sed -i "s|max_execution_time\s*=.*|max_execution_time = ${PHP_MAX_EXECUTION}|i" $pconf && \
 sed -i "s|memory_limit\s*=.*|memory_limit = ${PHP_MEMORY_LIMIT}|i" $pconf && \
