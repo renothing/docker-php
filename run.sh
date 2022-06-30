d2f80a4799275269f9a482c9c4669c263d2f88d0
renothing <renothing@users.noreply.github.com>
2022-06-30 11:54:34 +0800

    remove deprecated dependencies

--- run.sh
+++ run.sh
@@ -11,11 +11,11 @@ fconf=/etc/php${v}/php-fpm.conf
 [ $v == 5 ] && fconf1=/etc/php${v}/fpm.d/www.conf
 [ $v == 7 ] && fconf1=/etc/php${v}/php-fpm.d/www.conf
 FPM_INIT=$((FPM_MIN+1))
-sed -i "s|\s*error_log\s*=\s*/var/log/php-fpm.log|error_log = /dev/stderr|i" $fconf && \
-sed -i "s|;\s*emergency_restart_threshold\s*=.*|emergency_restart_threshold =${FPM_RESTART_THRESHOLD}|i" $fconf && \
-sed -i "s|;\s*emergency_restart_interval\s*=.*|emergency_restart_interval =${FPM_RESTART_INTERVAL}|i" $fconf && \
+sed -i "s|;\?\s*error_log\s*=.*|error_log = /dev/stderr|i" $fconf && \
+sed -i "s|;\?\s*emergency_restart_threshold\s*=.*|emergency_restart_threshold =${FPM_RESTART_THRESHOLD}|i" $fconf && \
+sed -i "s|;\?\s*emergency_restart_interval\s*=.*|emergency_restart_interval =${FPM_RESTART_INTERVAL}|i" $fconf && \
 #sed -i "s|;\s*process.max\s*=.*|process.max =${FPM_MAX}|i" $fconf && \
-sed -i "s|\s*error_log\s*=\s*/var/log/php-fpm.log|error_log = /dev/stderr|i" $fconf1 && \
+sed -i "s|;\?\s*php_admin_value\[error_log\]\s*=.*|php_admin_value\[error_log\] = /dev/stderr|i" $fconf1 && \
 sed -i "s|;*listen\s*=\s*127.0.0.1:9000|listen = ${PORT}|g" $fconf1 && \
 sed -i "s|;*pm.max_children\s*=.*|pm.max_children =${FPM_MAX}|i" $fconf1 && \
 sed -i "s|pm.start_servers\s*=.*|pm.start_servers =${FPM_INIT}|i" $fconf1 && \
@@ -32,7 +32,7 @@ sed -i "s|memory_limit\s*=.*|memory_limit = ${PHP_MEMORY_LIMIT}|i" $pconf && \
 sed -i "s|upload_max_filesize\s*=.*|upload_max_filesize = ${PHP_MAX_UPLOAD_SIZE}|i" $pconf && \
 sed -i "s|max_file_uploads\s*=.*|max_file_uploads = ${PHP_MAX_UPLOAD}|i" $pconf && \
 sed -i "s|post_max_size\s*=.*|post_max_size = ${PHP_MAX_POST}|i" $pconf && \
-sed -i "s|max_input_vars\s*=.*|max_input_vars = 5000|i" $pconf && \
+sed -i "s|;\?max_input_vars\s*=.*|max_input_vars = 5000|i" $pconf && \
 cp /usr/share/zoneinfo/${TIMEZONE} /etc/localtime && \
 echo "${TIMEZONE}" > /etc/TZ 
 stat -c "%U" /var/www|grep -q www-data || chown www-data:www-data /var/www
